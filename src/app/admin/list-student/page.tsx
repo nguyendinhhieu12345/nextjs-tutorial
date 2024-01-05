c8de4aca06dff25754b304876ff8a947ba99a5c5
nguyendinhhieu12345 <dinhhieunguyen07@gmail.com>
2024-01-06 02:54:51 +0700

    router

--- src/app/admin/list-student/page.tsx
+++ src/app/admin/list-student/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+import { useRouter } from "next/navigation";
+
 const ListStudent = () => {
-    return <>list student</>;
-  };
-  
-  export default ListStudent;
-  
+    const router = useRouter()
+    const handleRedirectAdmin = () =>{
+        router.push("/admin")
+    }
+  return (
+    <>
+      list student<br></br>
+      <button onClick={handleRedirectAdmin}>back to admin</button>
+    </>
+  );
+};
+
+export default ListStudent;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+import Link from "next/link";
+import { useEffect } from "react";
+
 const AdminPage = () => {
-  return <>admin page</>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      const data = await res.json()
+      console.log(data)
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      admin page <br></br>
+      <Link href={"/admin/list-student"}>Redirect in list student</Link>
+    </>
+  );
 };
 
 export default AdminPage;
